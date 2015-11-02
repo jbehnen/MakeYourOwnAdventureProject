b49826b06ee56e2e88de4c5608bdb82794aaff31
Julia Behnen <jmbehnen@gmail.com>
2015-11-02 11:24:00 -0800

    The beginning of the Great Change: Story and Story Element are now separate

--- app/src/main/java/behnen/julia/makeyourownadventure/model/Story.java
+++ app/src/main/java/behnen/julia/makeyourownadventure/model/Story.java
@@ -1,79 +1,31 @@
 package behnen.julia.makeyourownadventure.model;
 
-import android.annotation.SuppressLint;
-import android.util.Base64;
-import android.util.Log;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.Serializable;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
 /**
  * Created by Julia on 10/27/2015.
  */
-public final class Story implements Serializable {
+public final class Story {
 
     private static final String TAG = "Story";
-
-    public static final long serialVersionUID = 423548034214907L;
 
     public static final int START_ID = 0;
     private final String mAuthor;
     private final String mStoryId;
+    private final String mTitle;
+    private final String mDescription;
 
-    private String mTitle;
-    private String mDescription;
-    private Map<Integer, StoryElement> mStoryElements;
-
-
-    @SuppressLint("UseSparseArrays") // Sparse Arrays aren't easily put into JSON; may fix later.
     public Story(String theAuthor, String theId) {
-        this(theAuthor, theId, "", "", new HashMap<Integer, StoryElement>());
-        addStoryElement(new StoryElement(START_ID)); // 0 maps to the START story element
+        this(theAuthor, theId, "", ""); // 0 maps to the START story element
     }
 
     public Story(Story theOther) {
-        this(theOther.mAuthor, theOther.mStoryId, theOther.mTitle, theOther.mDescription,
-                theOther.mStoryElements);
+        this(theOther.mAuthor, theOther.mStoryId, theOther.mTitle, theOther.mDescription);
     }
 
-    public Story(String theAuthor, String theId, String theTitle, String theDescription,
-                  Map<Integer, StoryElement> theStoryElements) {
+    public Story(String theAuthor, String theId, String theTitle, String theDescription) {
         mTitle = theTitle;
         mStoryId = theId;
         mAuthor = theAuthor;
         mDescription = theDescription;
-        mStoryElements = new HashMap<>(theStoryElements);
-    }
-
-    public Story(String jsonStory) {
-        this(deserializeStory(jsonStory));
-        //this(parseJsonString(jsonStory));
-    }
-
-    public final void addStoryElement(StoryElement storyElement) {
-        mStoryElements.put(storyElement.getId(), storyElement);
-    }
-
-    public final boolean removeStoryElement(int theStoryElementId) {
-        if (theStoryElementId <= 1) {
-            return false;
-        }
-        mStoryElements.remove(theStoryElementId);
-        return true;
     }
 
     public String getAuthor() {
@@ -88,97 +40,12 @@ public final class Story implements Serializable {
         return mTitle;
     }
 
-    public void setTitle(String theTitle) {
-        mTitle = theTitle;
-    }
-
     public String getDescription() {
         return mDescription;
     }
 
-    public void setDescription(String theDescription) {
-        mDescription = theDescription;
-    }
-
-    public Map<Integer, StoryElement> getStoryElements() {
-        return new HashMap<>(mStoryElements);
-    }
-
-    public void setStoryElements(Map<Integer, StoryElement> theStoryElements) {
-        mStoryElements = new HashMap<>(theStoryElements);
-    }
-
-//    public String toJsonString() {
-//        JSONObject story;
-//        try {
-//            story = new JSONObject();
-//            story.put("author", mAuthor);
-//            story.put("storyId", mStoryId);
-//            story.put("title", mTitle);
-//            story.put("description", mDescription);
-//            story.put("elements", storyElementsToJsonString());
-//        } catch (JSONException e) {
-//            story = null;
-//        }
-//        return story.toString();
-//    }
-//
-//    private JSONObject storyElementsToJsonString() {
-//        Set<Integer> keys = mStoryElements.keySet();
-//        for
-//    }
-//
-//    private final Story parseJsonStory(String json) {
-//
-//    }
-//
-//    private final Map<Integer, StoryElement> parseJsonStoryElementsMap (String json) {
-//        Map<Integer, StoryElement> map = new HashMap<Integer, StoryElement>();
-//        try {
-//            JSONObject jsonElements = new JSONObject(json);
-//            Iterator<String> iter = jsonElements.keys();
-//            while (iter.hasNext()) {
-//                String key = iter.next();
-//                String storyElement = jsonElements.getString(key);
-//
-//            }
-//        } catch (JSONException e) {
-//            map = null;
-//        }
-//        return map;
-//    }
-
-    // Base64 encoding adapted from
-    // http://stackoverflow.com/questions/134492/how-to-serialize-an-object-into-a-string
-    public String getSerializedStory() {
-        String serialized;
-        try {
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(os);
-            oos.writeObject(this);
-            byte[] serializedBytes = os.toByteArray();
-            serialized = Base64.encodeToString(serializedBytes, Base64.URL_SAFE);
-            oos.close();
-            os.close();
-        } catch (IOException ioe) {
-           serialized = "";
-        }
-        return serialized;
-    }
-
-    private static final Story deserializeStory(String serialized) {
-        Story deserialized;
-        try {
-            byte[] serializedBytes = Base64.decode(serialized, Base64.URL_SAFE);
-            ByteArrayInputStream is = new ByteArrayInputStream(serializedBytes);
-            ObjectInputStream ois = new ObjectInputStream(is);
-            deserialized = (Story) ois.readObject();
-            ois.close();
-            is.close();
-        } catch (Exception e) {
-            Log.d(TAG, "Deserialization exception: " + e.toString());
-            deserialized = new Story("author", "id");
-        }
-        return deserialized;
+    public int newStoryElement() {
+        // TODO: implement
+        return -1;
     }
 }
--- app/src/main/java/behnen/julia/makeyourownadventure/model/StoryElement.java
+++ app/src/main/java/behnen/julia/makeyourownadventure/model/StoryElement.java
@@ -1,94 +1,93 @@
 package behnen.julia.makeyourownadventure.model;
 
-import android.graphics.Bitmap;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.Serializable;
 
 /**
  * Created by Julia on 10/27/2015.
  */
-public final class StoryElement implements Serializable {
+public final class StoryElement {
 
-    public static final long serialVersionUID = 45212343214451L;
-
-    private final int mId;
+    private final String mAuthor;
+    private final String mStoryId;
+    private final int mElementId;
     private final String mTitle;
     private final String mImageUrl;
     private final String mDescription;
     private final boolean mIsEnding;
-    private final int[] mChoiceIds;
-    private final String[] mChoiceDescriptions;
+    private final int mChoice1Id;
+    private final int mChoice2Id;
+    private final String mChoice1Text;
+    private final String mChoice2Text;
 
-    public StoryElement(int id) {
-        this(id, "", null, "", false, new int[2], new String[2]);
+    public StoryElement(String author, String storyId, int elementId) {
+        this(author, storyId, elementId, "", "", "", false, -1, -1, "", "");
     }
 
     // Constructs an ending
-    public StoryElement(int id, String title, String imageUrl, String description) {
-        this(id, title, imageUrl, description, true, new int[2], new String[2]);
+    public StoryElement(String author, String storyId, int elementId, String title,
+                        String imageUrl, String description) {
+        this(author, storyId, elementId, title, imageUrl, description, true, -1, -1, "", "");
     }
 
-    public StoryElement(int id, String title, String imageUrl, String description,
-                         boolean isEnding, int[] choiceIds,
-                         String[] theChoiceDescriptions) {
-        mId = id;
+    public StoryElement(String author, String storyId, int elementId, String title, String imageUrl,
+                        String description, boolean isEnding, int choice1Id, int choice2Id,
+                         String choice1Text, String choice2Text) {
+        mAuthor = author;
+        mStoryId = storyId;
+        mElementId = elementId;
         mTitle = title;
         mImageUrl = imageUrl;
         mDescription = description;
         mIsEnding = isEnding;
-        mChoiceIds = choiceIds.clone();
-        mChoiceDescriptions = theChoiceDescriptions.clone();
+        mChoice1Id = choice1Id;
+        mChoice2Id = choice2Id;
+        mChoice1Text = choice1Text;
+        mChoice2Text = choice2Text;
     }
 
-    public int getId() {
-        return mId;
+    public String toJson() {
+        JSONObject element;
+        try {
+            element = new JSONObject();
+            element.put("author", mAuthor);
+            element.put("storyId", mStoryId);
+            element.put("elementId", mElementId);
+            element.put("title", mTitle);
+            element.put("imageUrl", mImageUrl);
+            element.put("description", mDescription);
+            element.put("isEnding", mIsEnding);
+            element.put("choice1Id", mChoice1Id);
+            element.put("choice2Id", mChoice2Id);
+            element.put("choice1Text", mChoice1Text);
+            element.put("choice2Text", mChoice2Text);
+        } catch (JSONException e) {
+            element = new JSONObject();
+        }
+        return element.toString();
     }
 
-    public String getTitle() {
-        return mTitle;
+    private StoryElement parseJson(String json) {
+        StoryElement storyElement;
+        try {
+            JSONObject obj = new JSONObject(json);
+            String author = obj.getString("author");
+            String storyId = obj.getString("storyId");
+            int elementId = obj.getInt("elementId");
+            String title = obj.getString("title");
+            String imageUrl = obj.getString("imageUrl");
+            String description = obj.getString("description");
+            boolean isEnding = obj.getBoolean("isEnding");
+            int choice1Id = obj.getInt("choice1Id");
+            int choice2Id = obj.getInt("choice2Id");
+            String choice1Text = obj.getString("choice1Text");
+            String choice2Text = obj.getString("choice2Text");
+            storyElement = new StoryElement(author, storyId, elementId, title, imageUrl,
+                    description, isEnding, choice1Id, choice2Id, choice1Text, choice2Text);
+        } catch (JSONException e) {
+            storyElement = null;
+            e.printStackTrace();
+        }
+        return storyElement;
     }
-
-    public String getImageUrl() {
-        return mImageUrl;
-    }
-
-    public String getDescription() {
-        return mDescription;
-    }
-
-    public boolean isEnding() {
-        return mIsEnding;
-    }
-
-    public int[] getChoiceIds() {
-        return mChoiceIds.clone();
-    }
-
-    public String[] getChoiceDescriptions() {
-        return mChoiceDescriptions.clone();
-    }
-
-//    public JSONObject toJson() {
-//        JSONObject element;
-//        try {
-//            element = new JSONObject();
-//            element.put("id", mId);
-//            element.put("title", mTitle);
-//            element.put("imageUrl", mImageUrl);
-//            element.put("description", mDescription);
-//            element.put("choiceIds", new JSONArray(mChoiceIds));
-//            element.put("choiceDescriptions", new JSONArray(mChoiceDescriptions));
-//        } catch (JSONException e) {
-//            element = null;
-//        }
-//        return element;
-//    }
-//
-//    public StoryElement parseJson(String json) {
-//
-//    }
 }
